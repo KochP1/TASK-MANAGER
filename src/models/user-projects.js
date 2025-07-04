8d22760fbd23c8b3adf2fb552c8afb64107a575e
kochp1 <juanandreskochp@gmail.com>
2025-07-03 20:04:45 -0400

    feat: define UserProjects model with user_id, project_id, and assigned_at fields

--- src/models/user-projects.js
+++ src/models/user-projects.js
@@ -2,4 +2,25 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const UserProjects = sequelize.define('UserProjects', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: false
+    },
+    project_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    assigned_at: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        allowNull: true,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: 'user-projects',
+    timestamps: false,
+    underscored: false
 })
+
+module.export = UserProjects
