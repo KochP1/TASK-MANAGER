434a2cb1b5292ff8781e72fdf25c6a9114a94345
kochp1 <juanandreskochp@gmail.com>
2025-07-22 22:48:50 -0400

    feat(client): initialize Angular application structure with routing, components, and testing setup
    - Added app routing configuration.
    - Created main app component with title signal.
    - Implemented basic unit tests for app component.
    - Set up main entry point for Angular application.
    - Added global styles and index HTML file.
    - Configured TypeScript settings for application and tests.
    
    feat(server): integrate CORS middleware for API
    
    - Installed CORS package.
    - Configured CORS to allow requests from the client application.

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const authRouter = require('./routers/authRouter');
 const adminRouter = require('./routers/adminRouter');
 const userRouter = require('./routers/userRouter');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 
 // Rutas públicas
 app.use('/auth', authRouter);
