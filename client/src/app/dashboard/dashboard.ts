18478dc9f0316f04aacd0301f79d8f2be872580c
kochp1 <juanandreskochp@gmail.com>
2025-07-24 03:59:34 -0400

    Crate dashboard and render sidebar and navbar components

--- client/src/app/dashboard/dashboard.ts
+++ client/src/app/dashboard/dashboard.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Sidebar } from '../sidebar/sidebar';
+import { Navbar } from '../navbar/navbar';
+import { Content } from '../content/content';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
+  imports: [CommonModule, Sidebar, Navbar, Content],
+  template: '<div class="dashboard__wrapper"> <app-sidebar/> <div class="main-content"> <app-navbar/> <div class="content"> <app-content/> </div> </div> </div>',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
