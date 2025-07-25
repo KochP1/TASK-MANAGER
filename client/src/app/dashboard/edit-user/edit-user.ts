8355f5ef3d070eb1fa3fd0937874e24fa8498150
kochp1 <juanandreskochp@gmail.com>
2025-07-25 03:47:49 -0400

    Add edit user functionality: implement user form with validation, user retrieval, and update logic

--- client/src/app/dashboard/edit-user/edit-user.ts
+++ client/src/app/dashboard/edit-user/edit-user.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Auth } from '../../services/auth';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from '../../services/auth';
 
 @Component({
   selector: 'app-edit-user',
@@ -13,12 +14,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class EditUser {
   userForm: FormGroup;
-  projectId: number;
+  id: number;
+  user = signal<User | null>(null);
   private snackBar = inject(MatSnackBar)
   isLoading = signal(false);
   error = signal<string | null>(null);
-  constructor(private fb: FormBuilder, private authService: Auth, private route: ActivatedRoute) {
-      this.projectId = Number(this.route.snapshot.paramMap.get('id'));
+  constructor(private fb: FormBuilder, private authService: Auth, private route: ActivatedRoute, private router: Router) {
+      this.id = Number(this.route.snapshot.paramMap.get('id'));
       this.userForm = this.fb.group({
         name: ['', [
           Validators.required,
@@ -37,6 +39,80 @@ export class EditUser {
         role: ['user', [
           Validators.required
         ]]
-    })
+    });
+  }
+
+  ngOnInit(): void {
+    this.getUser();
+  };
+
+  getUser(): void {
+    this.isLoading.set(true);
+    this.error.set(null);
+    this.authService.getUserById(this.id).subscribe(
+      {
+        next: (user) => {
+          this.user.set(user);
+          this.userForm.patchValue({
+            name: this.user()?.name,
+            lastName: this.user()?.lastName,
+            email: this.user()?.email,
+            role: this.user()?.role
+          });
+          this.isLoading.set(false);
+        },
+        error: (err) => {
+          this.error.set('Error obteniendo el usuario');
+          this.isLoading.set(false);
+          this.showError('Error obteniendo el usuario: ' + err.message)
+        }
+      }
+    )
+  }
+
+  onSubmit() {
+      if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      this.snackBar.open('Por favor complete el formulario correctamente', 'X', {
+        duration: 3000
+      });
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.error.set(null);
+
+    this.authService.updateUser(this.id, this.userForm.value).subscribe({
+      next: (response) => {
+        this.snackBar.open('Usuario actualizado correctamente', 'X', {
+          duration: 3000
+        });
+
+        this.router.navigate(['/dashboard/users']);
+      },
+      error: (err) => {
+        this.error.set('Error al actualizar usuario');
+        this.snackBar.open(`Error al actualizar usuario: ${err.message}`, 'X', {
+          duration: 3000
+        });
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      }
+    });
+  }
+
+  private showError(message: string): void {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      panelClass: ['error-snackbar']
+    });
+  }
+
+  private showSuccess(message: string): void {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      panelClass: ['success-snackbar']
+    });
   }
 }
