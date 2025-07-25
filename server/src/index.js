fb5d57e515141c84cbe36fc750dd809520e56efd
kochp1 <juanandreskochp@gmail.com>
2025-07-25 15:23:40 -0400

    Config cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const authRouter = require('./routers/authRouter');
 const adminRouter = require('./routers/adminRouter');
 const userRouter = require('./routers/userRouter');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: process.env.SERVER_URL,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
