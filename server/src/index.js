b95004446966fba3a9562ae5ad3129a6179e0fe2
kochp1 <juanandreskochp@gmail.com>
2025-07-26 13:19:46 -0400

    changing url in cors to env url

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: process.env.SERVER_URL,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
